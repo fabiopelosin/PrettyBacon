59cde2c52a3211c06f894e7324e5e4fac9d8476a
Fabio Pelosin <fabiopelosin@gmail.com>
2013-09-09 15:07:50 +0200

    Improve output of elpased time 2

--- lib/pretty_bacon/spec_dox_output.rb
+++ lib/pretty_bacon/spec_dox_output.rb
@@ -47,7 +47,7 @@ module Bacon
         if elapsed_time <= 1
           elapsed_time_string = ''
         elsif elapsed_time >= 1000
-          elapsed_time_string = "(#{elapsed_time/1000} s)"
+          elapsed_time_string = "(#{(elapsed_time/1000.0).round(1)} s)"
         else
           elapsed_time_string = "(#{elapsed_time} ms)"
         end
